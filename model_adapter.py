ded6517d0102a8f93d8a691cfad269f5d54f9a3e
ShadiMah <shadi.m@dataloop.ai>
2024-02-19 17:31:37 +0200

    Updates following code review

--- model_adapter.py
+++ model_adapter.py
@@ -3,6 +3,7 @@ import torch
 import dtlpy as dl
 from mmdet.apis import init_detector, inference_detector
 import logging
+import subprocess
 
 logger = logging.getLogger('MMYolo')
 
@@ -11,29 +12,35 @@ logger = logging.getLogger('MMYolo')
                               name='model-adapter',
                               init_inputs={'model_entity': dl.Model})
 class MMYolo(dl.BaseModelAdapter):
-    def load(self, local_path, **kwargs):
-        config_name = 'yolox_s_fast_8xb8-300e_coco'
-        config_path = os.path.join(local_path, config_name)
-        if not os.path.exists(config_path):
-            exit_status = os.system(f'mim download mmyolo --config {config_name} --dest {config_path}')
-            if exit_status != 0:
-                logger.error("Failed to download model artifacts")
-                raise Exception("Failed to download model artifacts")
+    def __init__(self, model_entity: dl.Model):
+        self.model = None
+        self.confidence_thr = model_entity.configuration.get('confidence_thr', 0.4)
+        self.device = model_entity.configuration.get('device', None)
+        super(MMYolo, self).__init__(model_entity=model_entity)
 
-        files = os.listdir(config_path)
-        for file in files:
-            if file.endswith(".pth"):
-                checkpoint_file = os.path.join(config_path, file)
-            elif file.endswith(".py"):
-                config_file = os.path.join(config_path, file)
+    def load(self, local_path, **kwargs):
+        config_name = self.model_entity.configuration.get('model_name', 'yolox_s_fast_8xb8-300e_coco')
+        config_file = self.model_entity.configuration.get('config_file', 'yolox_s_fast_8xb8-300e_coco.py')
+        checkpoint_file = self.model_entity.configuration.get('checkpoint_file',
+                                                              'yolox_s_fast_8xb8-300e_coco_20230213_142600-2b224d8b.pth')
+
+        if not os.path.exists(config_file) or not os.path.exists(checkpoint_file):
+            logger.info("Downloading MMYolo artifacts")
+            download_status = subprocess.Popen(f'mim download mmyolo --config {config_name} --dest .',
+                                               stdout=subprocess.PIPE,
+                                               stderr=subprocess.PIPE,
+                                               shell=True)
+            download_status.wait()
+            (out, err) = download_status.communicate()
+            if download_status.returncode != 0:
+                raise Exception(f'Failed to download MMYolo artifacts: {err}')
+            logger.info(f"MMYolo artifacts downloaded successfully, Loading Model {out}")
 
         logger.info("MMYolo artifacts downloaded successfully, Loading Model")
-        if torch.cuda.is_available():
-            device = 'cuda:0'
-        else:
-            device = 'cpu'
-        self.model = init_detector(config_file, checkpoint_file, device=device)  # or device='cuda:0'
-        self.labels = self.model.dataset_meta.get('classes')
+        if self.device is None:
+            self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        logger.info(f"Loading model on device {self.device}")
+        self.model = init_detector(config_file, checkpoint_file, device=self.device)
         logger.info("Model and Classes Loaded Successfully")
 
     def predict(self, batch, **kwargs):
@@ -42,12 +49,15 @@ class MMYolo(dl.BaseModelAdapter):
         for image in batch:
             image_annotations = dl.AnnotationCollection()
             detections = inference_detector(self.model, image).pred_instances
-            detections = detections[detections.scores >= self.configuration.get('threshold', 0.4)]
+            detections = detections[detections.scores >= self.confidence_thr]
 
             for det in detections:
                 left, top, right, bottom = det.bboxes[0]
-                box = dl.Box(top=top, left=left, bottom=bottom, right=right, label=self.labels[det.labels])
-                image_annotations.add(annotation_definition=box,
+                image_annotations.add(annotation_definition=dl.Box(top=top,
+                                                                   left=left,
+                                                                   bottom=bottom,
+                                                                   right=right,
+                                                                   label=self.model_entity.labels[det.labels]),
                                       model_info={'name': self.model_entity.name,
                                                   'model_id': self.model_entity.id,
                                                   'confidence': det.scores})
